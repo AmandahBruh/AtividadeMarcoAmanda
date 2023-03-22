e8a0d657bf123534a9884414486669a3ba10faf6
Amanda <eunumseioqpordeemail@gmail.com>
2023-03-22 14:12:42 -0300

    waaaaaaa

--- src/screens/CadastrarScreen.jsx
+++ src/screens/CadastrarScreen.jsx
@@ -1,13 +1,50 @@
-import { View } from "react-native";
+import { View, TextInput } from "react-native";
 import { Text } from "react-native-paper";
 import styles from "../utils/styles";
 
 export default function CadastrarScreen() {
+  const [Nome, setNome] = useState();
+  const [Email, setEmail] = useState();
+  const [Senha, setSenha] = useState();
+
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
+        {" "}
         <h1>Sou a tela de cadastro</h1>
       </Text>
+
+      <TextInput
+        style={styles.input}
+        label="Nome"
+        placeholder="Digite seu email..."
+        multiline={false}
+      />
+
+      <TextInput
+        style={styles.input}
+        label="Email"
+        placeholder="Digite seu email..."
+        multiline={false}
+      />
+
+      <TextInput
+        style={styles.input}
+        label="Senha"
+        placeholder="Digite sua Senha..."
+        secureTextEntry={true}
+        multiline={false}
+      />
+
+      <TextInput
+        style={styles.input}
+        label="Confirmar Senha"
+        placeholder="Digite sua Senha..."
+        secureTextEntry={true}
+        multiline={false}
+      />
     </View>
   );
 }
