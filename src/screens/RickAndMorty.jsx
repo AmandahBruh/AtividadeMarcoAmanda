14cc1d3dd271657d2b8bd68cdfba42a5fad7e2cc
Amanda <eunumseioqpordeemail@gmail.com>
2023-03-29 16:37:18 -0300

    wawawawawa

--- src/screens/RickAndMorty.jsx
+++ src/screens/RickAndMorty.jsx
@@ -1,17 +1,31 @@
-import { View } from "react-native";
+import { useEffect, useState } from "react";
 import { Card, Paragraph, Title } from "react-native-paper";
+import { ScrollView } from "react-native-web";
 
 export default function RickAndMorty() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://rickandmortyapi.com/api/character")
+        .then((data) => data.json())
+        .then((dataJson) => setData(dataJson.results))
+        .catch((error) => console.error(error));
+    }, [])
+
   return (
-    <View>
-      <Card>
-        <Card.Title>Nome do episodio</Card.Title>
+    <ScrollView>
+        {data.map((item) => (
+        <Card key={item.name}>
+        <Card.Title>{item.name}</Card.Title>
         <Card.Content>
-            <Title>Título</Title>
-            <Paragraph>WAAAAAAA</Paragraph>
+            <Title>{item.air_date}</Title>
+            <Paragraph>{item.episode}</Paragraph>
         </Card.Content>
-        <Card.Cover source={{ uri: "https://picsum.photos/300" }}></Card.Cover>
+        <Card.Cover source={{ uri: item.image }}></Card.Cover>
       </Card>
-    </View>
+        ))}
+      
+    </ScrollView>
   );
 }
